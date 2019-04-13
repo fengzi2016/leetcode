33345b49dcd3ed9d2dd2aacf9ec6b46a436ceacb
guanyunfeng <1820166258@qq.com>
2019-04-13 22:48:17 +0800

    feat:add jd questions

--- test.cpp
+++ test.cpp
@@ -1,43 +1,81 @@
 #include<iostream>
 // #include<cstdio>
-// #include<algorithm>
+#include<algorithm>
 // #include<stdio.h>
 #include<vector>
-// #include<stack>
+#include<stack>
 // #include<unordered_map>
 // #include<unordered_set>
 // #include<numeric> 
 // #include<map>
 // #include<queue>
 using namespace std; 
-
- int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int> > dp(n+1, vector<int>(n+1,0));
-        dp[0][0] = 1;
-        int count = 0;
-        int max = INT_MIN;
-        for(int i = 0, j ; i < n - 1; i++){
-            int pre = nums[i];
-            for(j = i+1; j < n; j++){
-                if(nums[j]>=pre){
-                    pre = nums[j];
-                    dp[i][j] = dp[i][j - 1] + 1;
-                }else{
-                    dp[i][j] = dp[i][j - 1];
-                }
-                if(dp[i][j] > max){
-                  max = dp[i][j];
-                  count = 1;
-                }else if(dp[i][j] == max){
-                  count++;
-                }
-            } 
-        }
-        return count;
-    }
 int main(){
-  int a[] = {1,3,5,4,7};
-  vector<int> v(begin(a),end(a));
-  cout<<findNumberOfLIS(v);
+  int n;
+  cin >> n;
+  vector<int> numbers;
+  vector<int> rvnumbers;
+  stack<int> sta;
+  for(int i = 0; i < n; i++) {
+    int tmp;
+    cin >> tmp;
+    numbers.push_back(tmp);
+   // cout << numbers[i]<<endl;
+    sta.push(numbers[i]);
+  }
+
+  int count = 0,remember = 1;
+  for(int j = 0; j < n - 1; j++){
+    if(numbers[j] > numbers[j+1]){
+      count+=remember;
+      remember = 1;
+    }else if(numbers[j] < numbers[j+1]){
+      remember++;
+    }
+  }
+
+  while(!sta.empty()) {
+    int cur = sta.top();
+    sta.pop();
+    rvnumbers.push_back(cur);
+  }
+  int pre = rvnumbers[0];
+  int current,j = 0;
+  for(j = 1; j < n; j++){
+    current = rvnumbers[j];
+    if(current > pre){
+      cout<<j<<endl;
+      break;
+    }
+  }
+  cout<<j;
+  int result = n - j + 1;
+ // cout<< result;
 }
+
+// int main(){
+//   string s;
+//   cin>>s;
+//   int size  = s.size();
+//   if(size == 0) return 0;
+//   vector<int> dp(size,0);
+//   dp[0] = s[0] == '1' ? 1 : 0;
+//   for(int i = 1; i < size; i++){
+//     if(s[i] == '1'){
+//       dp[i] = dp[i-1] + 1;
+//     }
+//   }
+//   int beforemax = dp[0];
+//   for(int j = 1; j<size; j++){
+//     if(dp[j]==0){
+//       beforemax = dp[j-1];
+//       break;
+//     }
+//   }
+//   int firstandend = 0;
+//   firstandend = dp[size -1] + beforemax; 
+//   sort(dp.begin(),dp.end());
+//   int maxlen = dp[size - 1];
+//   int max = std::max(firstandend,maxlen);
+//   cout<<max;
+//  }
