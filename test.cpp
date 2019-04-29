2a8dd0765f8244837fa669290b9cff179e29c5c8
guanyunfeng <1820166258@qq.com>
2019-04-29 22:00:48 +0800

    feat: add new

--- test.cpp
+++ test.cpp
@@ -28,30 +28,50 @@
 //     cout<<fn(n);
 // }
 
-using namespace std; 
-vector< vector< vector<int> > > visited(n,vector<vector<int>>(n,vector<int>(n，-1)));
-int main() {
-  int n;
-  vector<int> x(n*n*n,0);
-  vector<int> y(n*n*n,0);
-  vector<int> z(n*n*n,0);
-  int maxp = 0;
-  int maxx,maxy,maxz;
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <limits>
+#include <math.h>
+#include <map>
 
-  for(int i = 0; i < n*n*n; i++) {
-    for(int j=0; j<4; j++){
-      int xp, yp, zp, pp;
-      cin << xp << yp << zp << pp;
-      x[xp] = xp;
-      y[yp] = yp;
-      z[zp] = zp;
-      visited[xp][yp][zp] = pp;
-      if(pp > maxp) {
-        maxx = xp;
-        maxy = yp;
-        
-        maxp = pp;
+using namespace std;
+vector<int> arr;
+
+/*请完成下面这个函数，实现题目要求的功能
+当然，你也可以不按照下面这个模板来作答，完全按照自己的想法来 ^-^ 
+******************************开始写代码******************************/
+int solution() {
+    map<int,int> threes;
+    int result = 0;
+    int i = 2;
+    int n = pow(3,i);
+    while(n <= pow(10,9)){
+          threes[n] = i++;
+          n = pow(3,i);
       }
+    for(int j = 0; j < arr.size(); j++){
+        if(threes.count(arr[j])){
+            result += threes[arr[j]];
+        }
     }
-  }
+    return result;
+
+}
+/******************************结束写代码******************************/
+
+
+int main() {
+    int res;
+	  int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+       int tmp;
+       cin >> tmp;
+       arr.push_back(tmp);
+    }
+    res = solution();
+    cout << res << endl;
+    return 0;
+
 }
--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-function solution(nums, n){
-  const result = [];
-  for(let i = 0; i < 2; i++){
-    let tmp = [];
-    tmp.push(i);
-    result.push(tmp);
-  }
-  return result;
+try {
+  setTimeout(()=>{
+    throw Error('我是错误')
+  })
+}catch(e){
+  console.log("into")
+  console.log(e)
 }
-console.log(solution());
